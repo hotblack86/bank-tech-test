aad9a6dfb03ab7fac34eb46dbbf2857e8db2d087
James Hotblack <james.c.hotblack@gmail.com>
2019-02-14 14:28:04 +0000

    Refactored withdraw and deposit methods to interect with Transaction class through TDD. Account spec passing for above features

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -7,7 +7,8 @@ require 'terminal-table'
 describe Account do
   let(:date) { Timecop.freeze Date.new(2019, 2, 11) }
   let(:account) { Account.new }
-  let(:transaction) { Transaction.new(500, date) }
+  let(:transaction1) { Transaction.new(500, date) }
+  let(:transaction2) { Transaction.new(200, date) }
   
 
   it 'Initializes with a balance of zero' do
@@ -19,13 +20,13 @@ describe Account do
   end
 
   it 'Deposit method adds money to balance' do
-    account.deposit(transaction)
+    account.deposit(transaction1)
     expect(account.balance).to eq(500)
   end
 
   it 'Withdraw method subtracts money from balance' do
-    account.deposit(500)
-    account.withdraw(200)
+    account.deposit(transaction1)
+    account.withdraw(transaction2)
     expect(account.balance).to eq(300)
   end
 
